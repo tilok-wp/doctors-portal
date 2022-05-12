013afea4c0d7455d0f36e91259035a4ecb9a44ec
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-12 08:11:49 +0600

    Create component folder and page, Create navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Appionment from './components/pages/Appionment/Appionment';
+import Contact from './components/pages/Contact/Contact';
+import About from './components/pages/Home/About/About';
+import Home from './components/pages/Home/Home';
+import Login from './components/pages/Login/Login';
+import NotFound from './components/pages/NotFound/NotFound';
+import Register from './components/pages/Register/Register';
+import Reviews from './components/pages/Reviews/Reviews';
+import Header from './components/shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appiontment' element={<Appionment></Appionment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+
     </div>
+
   );
 }
 
---
+++ src/components/pages/Appionment/Appionment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appionment = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Appionment;
